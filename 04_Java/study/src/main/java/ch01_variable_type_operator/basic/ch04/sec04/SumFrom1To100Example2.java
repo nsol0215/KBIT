4fab366ba59fda6e0e64ed6cc8d025a66315eb8d
nsol0215 <nsol0215@gmail.com>
2026-04-16 17:36:40 +0900

    Feat. 04 java 추가

---
+++ 04_Java/study/src/main/java/ch01_variable_type_operator/basic/ch04/sec04/SumFrom1To100Example2.java
@@ -0,0 +1,12 @@
+package main.java.ch01_variable_type_operator.basic.ch04.sec04;
+
+public class SumFrom1To100Example2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 1;
+        while (num <= 100){
+            sum += num;
+            num ++;
+        }
+    }
+}
