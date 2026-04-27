11ecbdc50560f9dfc66712485f6bbbdccef3b7fd
nsol0215 <nsol0215@gmail.com>
2026-04-27 09:47:44 +0900

    feat.확인

--- 04_Java/src/main/java/ch08/sec04/Audio.java
+++ 04_Java/src/main/java/ch08/sec04/Audio.java
@@ -12,10 +12,10 @@ public class Audio implements RemoteControl {
     }
 
     public void setVolume(int volume){
-        if(volume>RemoteControl.MAX_VOLUME){
-            this.volume=RemoteControl.MAX_VOLUME;
-        }else if(volume<RemoteControl.MIN_VOLUME){
-            this.volume=RemoteControl.MIN_VOLUME;
+        if(volume> MAX_VOLUME){
+            this.volume= MAX_VOLUME;
+        }else if(volume< MIN_VOLUME){
+            this.volume= MIN_VOLUME;
 
         }else{
             this.volume=volume;
--- 04_Java/src/main/java/ch08/sec04/Television.java
+++ 04_Java/src/main/java/ch08/sec04/Television.java
@@ -13,10 +13,10 @@ public class Television implements RemoteControl{
 
 
     public void setVolume(int volume){
-        if(volume>RemoteControl.MAX_VOLUME){
-            this.volume=RemoteControl.MAX_VOLUME;
-        }else if(volume<RemoteControl.MIN_VOLUME){
-            this.volume=RemoteControl.MIN_VOLUME;
+        if(volume> MAX_VOLUME){
+            this.volume= MAX_VOLUME;
+        }else if(volume< MIN_VOLUME){
+            this.volume= MIN_VOLUME;
 
         }else{
             this.volume=volume;
